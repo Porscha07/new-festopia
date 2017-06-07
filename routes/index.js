a01b9ab78ea5a29558f59073d2ddef825b3382ef
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-06-07 15:12:08 -0400

    Added nom request and made index.js display both index.ejs(‘/‘) and login.ejs(‘/login’). Also prepared index.js for when we have a config and a mySQL server up and running.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+// var config = require('../config/config');
+var bcrypt = require('bcrypt-nodejs');
+
+// var mysqpl = require('myspl');
+// var connection = mysql.createConnection({
+//     host: config.sql.host,
+//     user: config.sql.user,
+//     password: config.sql.password,
+//     database: config.sql.database
+// });
+
+// connection.connect();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('sign', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/sign', function(req, res) {
+    res.render('sign', {});
 });
 
 module.exports = router;
