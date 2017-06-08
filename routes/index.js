ab162d8bbb9a695f208018516649c3035b15c59c
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-06-08 11:59:54 -0400

    Fixed index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,7 @@ var request = require('request');
 // var config = require('../config/config');
 var bcrypt = require('bcrypt-nodejs');
 
-<<<<<<< HEAD
-// var mysqpl = require('mysql');
-=======
 // var mysql = require('mysql');
->>>>>>> 312bca93457da68ccb79a342ed00d6a6d46e0078
 // var connection = mysql.createConnection({
 //     host: config.sql.host,
 //     user: config.sql.user,
@@ -19,33 +15,24 @@ var bcrypt = require('bcrypt-nodejs');
 // connection.connect();
 
 
-<<<<<<< HEAD
-//GET home page. 
-=======
+
 /* GET home page. */
 
->>>>>>> 312bca93457da68ccb79a342ed00d6a6d46e0078
 router.get('/', function(req, res, next) {
    res.render('index', { title: 'Express' });
 });
 
-<<<<<<< HEAD
-router.get('sign', function(req, res) {
+router.get('/sign', function(req, res) {
     res.render('sign', {});
 });
-=======
-// router.get('/sign', function(req, res) {
-//     res.render('sign', {});
-// });
 
-// router.get('/register', function(req, res) {
-//     res.render('register', {});
-// });
+router.get('/register', function(req, res) {
+    res.render('register', {});
+});
 
-// router.get('/', function(req, res) {
-//     res.render('cat-template', {});
-// });
->>>>>>> 312bca93457da68ccb79a342ed00d6a6d46e0078
+router.get('/cat-template', function(req, res) {
+    res.render('cat-template', {});
+});
 
 
 
