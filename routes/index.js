a2924e0c2f8749e667a5e72f61c7914d98e778e2
Porscha07 <porscha.uga@gmail.com>
2017-06-10 19:17:04 -0400

    altered home page, added images,

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ var bcrypt = require('bcrypt-nodejs');
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: config.sql.host,
-    user: config.sql.user,
-    password: config.sql.password,
-    database: config.sql.database
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
 });
 
 
@@ -48,11 +48,36 @@ router.get('/film', function(req, res){
     	res.render('cat-template', {
     		message:message,
     		searchArray:results,
-    		category: "film"
+    		category: "film",
+            title: "FILM"
     	});
     });
 
 });
+//getting the culture results and displaying them in template
+router.get('/culture', function(req, res){
+    var message = req.query.msg;
+        //if results not found in database, create message to show on template page, else display no message
+        if(message == "sorryfestivalnotfound"){
+            message="Sorry, festival not found!"    
+        }else if (message == null){
+            message = " "
+        }
+        //selecting everything from culture in table Categories 
+        var selectQuery = "SELECT * FROM Categories WHERE Category = 'Culture'";
+        connection.query(selectQuery,(error,results)=>{
+            console.log(results)
+        //showing message and results for Culture category template page
+        res.render('cat-template', {
+            message:message,
+            searchArray:results,
+            category: "culture",
+            title: "CULTURE"
+        });
+    });
+
+});
+//getting the music results and displaying them in template
 router.get('/music', function(req, res){
     var message = req.query.msg;
         //if results not found in database, create message to show on template page, else display no message
@@ -69,13 +94,39 @@ router.get('/music', function(req, res){
         res.render('cat-template', {
             message:message,
             searchArray:results,
-            category: "music"
+            category: "music",
+            title:"MUSIC"
         });
     });
 
 });
+//searching for all festivals
+router.post('/search', function(req, res){
+    //getting input data and turning them into variables to simplify search and pass into query  
+    var name = req.body.name;
+    var date = req.body.date;
+    var family = req.body.family;
+    var free = req.body.free;
+    //selecting specific data from table Categories in our database 
+    var selectQuery = "SELECT * FROM Categories WHERE Name = ? OR Date = ? OR Family = ?";
+    // var selectQuery = "SELECT * FROM Categories WHERE Category = Music OR Date = ? OR Family = ? OR Free = ?";
+    console.log(free);
+    connection.query(selectQuery,[name,date,family,free],(error,results)=>{
+        //if results are not found in the database, redirect to page w/ message > not found
+        if(results.length == 0){
+            res.redirect('/cat-template?msg=sorryfestivalnotfound')
+        }else{
+            res.render('cat-template', {
+                //creating keys to display in category template page 
+                message: null,
+                searchArray: results,
+                category: "music"
+            })
+        }
+    });
+});
 
-//getting film results and displaying them in template
+//getting art results and displaying them in template
 router.get('/art', function(req, res){
 	var message = req.query.msg;
     	//if results not found in database, create message to show on template page, else display no message
@@ -92,8 +143,32 @@ router.get('/art', function(req, res){
     	res.render('cat-template', {
     		message:message,
     		searchArray:results,
-    		category: "art"
+    		category: "art",
+            title:"ART"
     	});
+    });
+
+});
+//getting food results and displaying them in template
+router.get('/food', function(req, res){
+    var message = req.query.msg;
+        //if results not found in database, create message to show on template page, else display no message
+        if(message == "sorryfestivalnotfound"){
+            message="Sorry, festival not found!"    
+        }else if (message == null){
+            message = " "
+        }
+        //selecting everything from Food in table Categories 
+        var selectQuery = "SELECT * FROM Categories WHERE Category = 'Food'";
+        connection.query(selectQuery,(error,results)=>{
+            console.log(results)
+        //showing message and results for FILM category template page
+        res.render('cat-template', {
+            message:message,
+            searchArray:results,
+            category: "food",
+            title:"FOOD"
+        });
     });
 
 });
@@ -115,11 +190,13 @@ router.get('/cat-template', function(req, res){
 			//showing message and results on category template page
     	res.render('cat-template', {
     		message:message,
-    		searchArray:results
+    		searchArray:results,
+            category: "music"
     	});
     });
 });
 
+//front page search ONLY
 router.post('/search', function(req, res){
 	//getting input data and turning them into variables to simplify search and pass into query  
 	var name = req.body.name;
@@ -129,7 +206,7 @@ router.post('/search', function(req, res){
 	//selecting specific data from table Categories in our database 
 	// var selectQuery = "SELECT * FROM Categories WHERE Name = ? OR Date = ? OR Family = ?";
     var selectQuery = "SELECT * FROM Categories WHERE Name = ? OR Date = ? OR Family = ? OR Free = ?";
-	connection.query(selectQuery,[name,date,family],(error,results)=>{
+	connection.query(selectQuery,[name,date,family,free],(error,results)=>{
 		//if results are not found in the database, redirect to page w/ message > not found
 		if(results.length == 0){
 			res.redirect('/cat-template?msg=sorryfestivalnotfound')
@@ -137,7 +214,9 @@ router.post('/search', function(req, res){
 			res.render('cat-template', {
 				//creating keys to display in category template page 
 				message: null,
-				searchArray: results})
+				searchArray: results,
+                category: "music"
+            })
 		}
 	});
 });
